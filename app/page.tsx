5143dd820448eac80b2891adc15034678fa62070
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-10-16 21:56:48 -0300

    Very simple -- and raw -- responsible layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 
+"use client"
+import { useEffect } from 'react';
 import Link from 'next/link';
 
 function Video({episode, description, link, date}) {
     return (
-	<div className="flex flex-row w-full p-5 rounded-lg  transition-all bg-white hover:shadow-xl hover:z-10 hover:-translate-y-3 cursor-pointer">
-	    <Link href={link} target="blank" className="flex flex-row w-full">
-	        <div className="flex flex-col justify-center items-center mr-10 min-h-full flex flex-col justify-center items-center mr-10">
-	            <div className="w-100 h-60 bg-[#ff0e26] rounded-lg flex flex-col justify-center"><img src="/full.jpeg" width="100%" className="" /></div>
+	<div className="lg:flex lg:flex-row md:flex md:flex-col w-full p-5 rounded-lg  transition-all bg-white hover:shadow-xl hover:z-10 hover:-translate-y-3 cursor-pointer">
+	    <Link href={link} target="blank" className="lg:flex lg:flex-row md:flex md:flex-col w-full">
+	        <div className="flex flex-col mr-10 min-h-full justify-center items-center mr-10 lg:min-w-[40%] md:min-w-full">
+	            <div className="lg:w-100 lg:h-60 md:w-60 bg-[#ff0e26] rounded-lg flex flex-col justify-center"><img src="/full.jpeg" width="100%" className="" /></div>
 	        </div>
-	        <div className="w-full h-full flex flex-col justify-center text-black">
+	        <div className="w-full h-full flex flex-col justify-center text-black lg:mt-0 mt-5">
 	             <span className="tracking-[.20em] text-xl font-black font-(family-name:--font-black)">
 	                 Episódio #{episode}
 	             </span>
@@ -41,29 +43,76 @@ function Profile({name}) {
 
 
 function TopBar() {
+    
+    let nav_bar_opened = false;
+    
+    const update_navbar = () => {
+	if (nav_bar_opened) {
+	    if (navbar.classList.contains("-left-100")) {
+		navbar.classList.remove("-left-100")
+		navbar.classList.add("left-0");
+	    }
+	}
+	else {
+	    if (navbar.classList.contains("left-0")) {
+		navbar.classList.remove("left-0");
+		navbar.classList.add("-left-100");
+	    }
+	}
+    }
+    
+    useEffect(() => {
+	const text_container = document.getElementById("topbar-text-container");
+	const circle_logo = document.getElementById("circle_logo");
+	const navbar = document.getElementById("navbar");
+	
+	if (window.screen.width <= 576) {
+	    text_container.classList.add("hidden");
+	    circle_logo.addEventListener("click", () => {
+		nav_bar_opened = !nav_bar_opened;
+		update_navbar();
+	    });
+	} else {
+	    navbar.classList.add("hidden");
+	}
+    }, []);
+    
     return (
-	<nav className="shadow-xl flex flex-row items-center p-5 bg-[#f42f39] text-white z-10">
+	<nav className="shadow-xl lg:flex lg:flex-row md:flex md:flex-col items-center p-5 bg-[#f42f39] text-white z-10">
 	    <div className="w-full h-full">
 	        <img
+	            id="circle_logo"
 	            src="/circle_logo.png" 
 	            width={80}
 	            height={80}
 	            alt="volume25 circle logo"
 	        />
 	    </div>
-	    <nav className="flex flex-row items-center gap-5">
+	    <nav id="topbar-text-container" className="flex flex-row items-center gap-5">
 	        <Link className="w-35 tracking-[.25em] text-center text-white font-(family-name:--font-black)" href="#about">Sobre</Link> 
 	        <Link className="w-35 tracking-[.25em] text-center text-white font-(family-name:--font-black)" href="#episodes">Episódios</Link> 
 	        <Link className="w-35 tracking-[.25em] text-center text-white font-(family-name:--font-black)" href="#authors">Autores</Link> 
 	        <Link className="w-45 tracking-[.25em] text-center text-white font-(family-name:--font-black)" href="/mp">Mundo Perdido...</Link> 
+	    </nav>
+	    <nav id="navbar" className="z-20 absolute flex flex-col justify-start bg-[#f42f39] shadow-lg h-screen w-screen -left-100 top-20 p-5 transition-all lg:hidden">
+	        <Link className="w-35 tracking-[.25em] text-left text-white font-(family-name:--font-black) mt-3 mb-3" onClick={() => {nav_bar_opened = false; update_navbar(); }} href="#about">Sobre</Link> 
+	        <Link className="w-35 tracking-[.25em] text-left text-white font-(family-name:--font-black) mt-3 mb-3" onClick={() => {nav_bar_opened = false; update_navbar(); }} href="#episodes">Episódios</Link> 
+	        <Link className="w-35 tracking-[.25em] text-left text-white font-(family-name:--font-black) mt-3 mb-3" onClick={() => {nav_bar_opened = false; update_navbar(); }} href="#authors">Autores</Link> 
+	        <Link className="w-45 tracking-[.25em] text-left text-white font-(family-name:--font-black) mt-3 mb-3" onClick={() => {nav_bar_opened = false; update_navbar(); }} href="/mp">Mundo Perdido...</Link>
 	    </nav>
 	</nav>
     );
 }
 
 function Footer() {
+    useEffect(() => {
+	const footer = document.getElementById("footer");
+	if (window.screen.width <= 576) {
+	    footer.classList.add("hidden");
+	}
+    }, []);
     return (
-	<section className="p-5 bg-[#f42f39] flex flex-col justify-center items-center mt-15">
+	<section id="footer" className="p-5 bg-[#f42f39] flex flex-col justify-center items-center mt-15">
 	    <div className="h-full w-1/2 flex flex-row gap-15 justify-center items-center mb-8 mt-8">
 	        <Link href="/"><img src="logo.jpeg" className="w-52 h-30" alt="Volume 25" /></Link>
 	        <Link href="https://www2.unesp.br/" target="blank"><img src="Logo_Unesp.svg.png" className="w-45 h-15" alt="Unesp Bauru" /></Link>
@@ -79,12 +128,24 @@ function Footer() {
 }
 
 function Content() {
+    useEffect(() => {
+	const about_image = document.getElementById("about-image");
+	
+	if (window.screen.width <= 576) {
+	    about_image.classList.add("hidden");
+	} else {
+	    if (about_image.classList.contains("hidden")) {
+		about_image.classList.remove("hidden");
+	    }
+	    
+	}
+    }, []);
     return (
 	<section className="h-full overflow-auto">
 	    <section className="bg-[#f42f39] h-full flex flex-col">
 	        <TopBar />
-	        <div className="pl-20 h-full flex flex-row justify-center items-center">
-	            <img src="/logo.jpeg" width="30%" alt="logo" className="" />
+	        <div className="h-full flex flex-row justify-center items-center">
+	            <img src="/logo.jpeg" alt="logo" className="lg:w-[30%] md:w-[50%]" />
 	        </div>
 	        <div className="min-w-full flex flex-row justify-end items-center pr-3">
 	            <Link href="https://www.instagram.com/volume_025?igsh=MTN1enpsOWF3MW05YQ==" target="blank" className="hover:-translate-y-2 cursor-pointer transition-all"><img src="/instagram-logo.png" width="30px" /></Link>
@@ -93,30 +154,30 @@ function Content() {
 	        </div>
 	    </section>
 	    
-	    <section id="about" className="h-auto flex flex-row overflow-hidden">
-	         <div className="w-full h-full">
-	            <img src="/about.jpeg" className="relative " />
+	    <section id="about" className="h-auto lg:flex lg:flex-row md:flex md:flex-col overflow-hidden">
+	         <div id="about-image" className="min-w-[50%] min-h-auto">
+	            <img src="/about.jpeg" className="relative" />
 	         </div>
-	         <div className="p-5 pl-10 pr-10 flex flex-col justify-center w-full min-h-full bg-white shadow-xl shadow-x1/30 shadow-black z-10">
-	             <p className="tracking-[.20em] text-xl font-black mb-5 mt-5 font-(family-name:--font-black)">
+	         <div className="p-5 pl-10 pr-10 flex flex-col justify-center lg:min-w-full lg:min-h-full md:min-w-screen md:min-h-auto bg-white shadow-xl shadow-x1/30 shadow-black z-10">
+	             <p className="w-full tracking-[.20em] text-xl font-black mb-5 mt-5 font-(family-name:--font-black)">
 	                 Sobre...
 	             </p>
-	             <p className="text-sm/7 text-justify mb-2 mt-2">
+	             <p className="lg:w-[45%] text-sm/7 text-justify mb-2 mt-2">
 	                 Volume 25 é um programa criado pelos estudantes do segundo semestre de Jornalismo da Unesp!
 	             </p>
-	             <p className="text-sm/7 text-justify mb-2 mt-2">
+	             <p className="lg:w-[45%] text-sm/7 text-justify mb-2 mt-2">
 	                 Com o principal objetivo de garantir que todos possam conhecer e atuar em cada área da produção de um programa ao
 	                 vivo, os estudantes revezam as funções a cada episódio -- desde a produção, mídias socias, técnica e reportagem. 
 	                 Dessa forma, todos os grupos participarão de todas as funções.
 	             </p>
-	             <p className="text-sm/7 text-justify mb-2 mt-2">
+	             <p className="lg:w-[45%] text-sm/7 text-justify mb-2 mt-2">
 	                 Cada edição do programa irá focar em um tema específico escolhido pelos apresentadores da semana, com reportagens,
 	                 e entrevistas ao vivo com convidados especiais.
 	             </p>
 	         </div>
 	    </section>
 	    
-	    <section id="episodes" className="p-20 h-auto">
+	    <section id="episodes" className="lg:p-20 h-auto">
 	        <Video date="09/09/2025" episode={1} description="Estreando já como um clássico, o Volume 25 começa suas atividades com um bate-papo sobre a UEFA Champions League, a famosa Liga dos Campeões da Europa! A edição contou com a apresentação de Enzo Kato,  Mauricio Júnior e Pedro Oliveira, assim como com a participação especial de Antônio Vinicius, jornalista formado pela Unesp, lider da editoria de esportes da rádio Aure Verde Brasil e produtor do programa esportivo Aure Verde na área. " link="https://www.youtube.com/live/bN8k6hQGx_Q?si=81_Xpa9Slwjy9dYA" />
 	        <Video date="23/09/2025" episode={2} description="Já no segundo episódio, o Volume 25 fala sobre a 49ª Amostra Internacional de Cinema em São Paulo, tendo como convidado especial o diretor e cineasta independente Marcelo Felipe Sampaio e com a apresentação de Bruna Arcanjo, Clara Moraes e Danielle Ribeiro. O episódio também contou com atualizações e cobertura dos fontes vendavais acontecidos na semana do programa." link="https://www.youtube.com/live/u-bKSx0JXVs?si=ZRY5U9NDigpSkx8N"/>
 	        <Video date="07/10/2025 - Última edição." episode={3} description="O terceiro episódio do Volume 25 aborda o pertinente tema sobre a proibição de celulares em escolas e instituições de ensino, debatendo os lados positivos e negativos da restrição. O programa contou com os convidados Dariel Carvalho, professor e especialista em tecnologia educacional, Katia Chedid, psicopedagoga e neurocientista, e Karina Martins, diretora e representante da escola Professor Antônio Guedes de Azevedo. A apresentação foi realizada por Maria Eduarda Deodato e Helena Singilo." link="https://www.youtube.com/live/k7fffm6vc0k?si=7O37tKeGr1YyPIEh" />
